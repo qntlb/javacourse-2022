dbd204f177180f864df59982917ac38333302caf
Andrea Mazzon <mazzon@math.lmu.de>
2022-04-15 15:06:55 +0200

    First two sessions

---
+++ src/main/java/com/andreamazzon/session1/oopdivisible/MainDivisible.java
@@ -0,0 +1,14 @@
+package com.andreamazzon.session1.oopdivisible;
+
+/**
+ * This class tests if an integer is divisible by another integer
+ *
+ * @author Andrea Mazzon
+ */
+public class MainDivisible {
+
+	public static void main(String[] args) {
+
+		// your code here
+	}
+}
---
+++ src/main/java/com/andreamazzon/session1/primenumbers/PrimesCheck.java
@@ -0,0 +1,12 @@
+package com.andreamazzon.session1.primenumbers;
+
+/**
+ * This class is used to check if a number is prime, by a simple for method with
+ * an if statement
+ *
+ * @author: Andrea Mazzon
+ */
+public class PrimesCheck {
+
+	//your code here
+}
