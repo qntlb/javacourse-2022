ec3ec6fc9753a6187dfe452dd9c2b6606c2f2ba9
Andrea Mazzon <mazzon@math.lmu.de>
2022-04-28 18:05:43 +0200

    Solution to exercises

---
+++ src/main/java/com/andreamazzon/exercises/modifiedgenerator/LinearCongruentialGeneratorWithNegativeNumbers.java
@@ -0,0 +1,47 @@
+package com.andreamazzon.exercises.modifiedgenerator;
+
+import com.andreamazzon.session3.encapsulation.lazyinitialization.LinearCongruentialGenerator;
+
+/**
+ * This class generates pseudo random integers by a linear congruential
+ * generator. The difference with LinearCongruentialGenerator is indeed that
+ * here we also accept negative numbers. Thus we make this class inherit from
+ * LinearCongruentialGenerator, just overriding one method, subtracting modulus
+ * from the numbers generated in the method of the parent class. Note that we
+ * override the method getRandomNumberSequence() first calling the version of
+ * the parent class, and then modifying its output. The call of the version of
+ * the parent class is done by super.nameOfTheMethod. Also note that we don't
+ * have not to modify any access modifier of the parent class. It inherits all
+ * the other public methods.
+ *
+ * @author Andrea Mazzon
+ *
+ */
+public class LinearCongruentialGeneratorWithNegativeNumbers extends LinearCongruentialGenerator {
+
+	private int flag = 0;// it gets 1 the first time we subtract modulus from the generated numbers
+
+	// the constructor is just the one of the parent class
+	public LinearCongruentialGeneratorWithNegativeNumbers(int numberOfPseudoRandomNumbers, long seed) {
+		super(numberOfPseudoRandomNumbers, seed);
+	}
+
+	/**
+	 * getter method for the sequence of pseudo random integer numbers (they can
+	 * also be negative).
+	 *
+	 * @return the sequence of (possibly negative) pseudo random integer numbers
+	 */
+	@Override
+	public long[] getRandomNumberSequence() {
+
+		long[] sequence = super.getRandomNumberSequence();// in this way I call the not overloaded method!
+		if (flag == 0) { // it has to be done just once
+			for (int indexOfInteger = 0; indexOfInteger < getNumberOfPseudoRandomNumbers(); indexOfInteger++) {
+				sequence[indexOfInteger] -= getModulus() / 2;
+			}
+			flag = 1;
+		}
+		return sequence;
+	}
+}
---
+++ src/main/java/com/andreamazzon/exercises/simulators/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * The code in this package gives an example of a combination of inheritance and
+ * composition. Indeed, we have an abstract class StochasticProcessSimulator
+ * which provides the implementation of some methods that can be called to 
+ * get the paths, the simulated value at a given time and the average value at
+ * given time of a general stochastic process. However, all these methods are
+ * based on the generation of the specific values of the process, given by a
+ * method generateRealizations() whose implementation cannot be given in this
+ * base class, but only in classes that extend it. For this reason, this is
+ * defined to be abstract. In particular, two classes inherit from
+ * StochasticProcessSimulator, that is, BinomialModelSimulator and
+ * TrinomialModelSimulator. In this way, we have what we want: the classes
+ * BinomialModelSimulator and TrinomialModelSimulator inheriting from
+ * StochaticProcessUser directly inherit the methods already implemented there,
+ * so that we don't have to rewrite them, and they provide the specific
+ * implementation of generateRealizations(). They do this by composition: they
+ * have a field that is an object of a class that generate pseudo random
+ * integers, and which is used in the implementation of generateRealizations().
+ */
+package com.andreamazzon.exercises.simulators;
